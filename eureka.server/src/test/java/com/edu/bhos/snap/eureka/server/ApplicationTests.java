20350fcb60e74ea2cfd3cf6bde474e6b3bb5912e
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-06-30 15:15:55 +0400

    Eureka Server added

---
+++ eureka.server/src/test/java/com/edu/bhos/snap/eureka/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.bhos.snap.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
