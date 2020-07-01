fdd53591629b4d03bbe6c2c186080a70169612e8
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-07-01 11:34:17 +0400

    feign client service added

---
+++ feign-client/src/test/java/com/edu/bhos/snap/feignclient/FeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.bhos.snap.feignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- user-service/src/main/java/com/edu/bhos/snap/userservice/controller/abstracts/IUserController.java
+++ user-service/src/main/java/com/edu/bhos/snap/userservice/controller/abstracts/IUserController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public interface IUserController {
     
-    @GetMapping("/")
+    @GetMapping("")
     public String index();
 }
