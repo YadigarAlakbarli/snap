60dba2c4ddbad95221c31c2dd9dc35c50863cb1f
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-07-05 00:33:46 +0400

    demo user

--- postservice/src/main/java/com/edu/bhos/snap/postservice/controller/PostController.java
+++ postservice/src/main/java/com/edu/bhos/snap/postservice/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.edu.bhos.snap.postservice.controller;
 
 import com.edu.bhos.snap.postservice.controller.abstracts.IPostController;
+import com.edu.bhos.snap.postservice.feign.FeignClientUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/post-service")
 public class PostController implements IPostController {
 
+    @Autowired
+    FeignClientUserService feignUserService;
+
     @Value("${message}")
     String message;
 
     @GetMapping("/index")
     public String index(){
      return message;
+    }
+
+    @GetMapping("/demouser")
+    public Object getDemoUser(){
+        return feignUserService.getDemoUser();
     }
 }
