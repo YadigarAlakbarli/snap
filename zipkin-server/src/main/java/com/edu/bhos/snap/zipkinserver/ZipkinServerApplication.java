0daec173cf92edbaa28eae83cb6e3009938fdc55
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-07-07 12:03:27 +0400

    Logging file added

--- zipkin-server/src/main/java/com/edu/bhos/snap/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/edu/bhos/snap/zipkinserver/ZipkinServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin2.server.internal.EnableZipkinServer;
 
+
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
